816e42a2a2f5cb2e186b27fa95aaf4c7d996d255
voleresistor <voleresistor@gmail.com>
2016-06-20 10:57:38 -0500

    Initial Build

---
+++ 1.0.0/xSCCM.psm1
@@ -0,0 +1,82 @@
+<#
+    Custom functions for common operations in SCCM and MDT.
+    
+    Created 06/20/16
+    
+    Changelog:
+        06/20/16 - v 1.0.0
+            Initial build
+            Added Split-DriverSource
+#>
+
+#region Split-DriverSource 
+function Split-DriverSource
+{
+    param
+    (
+        [string]$SourcePath,
+        [string]$DestPath,
+        [switch]$Verbose
+    )
+    
+    $DestFileCount = 0
+    $SourceFileCount = 0
+    
+    if (!(Test-Path -Path $DestPath))
+    {
+        New-Item -Path $DestPath -ItemType Directory | Out-Null
+    }
+    
+    $FullSource = Get-ChildItem -Path $SourcePath -Recurse
+    $SourceFileCount = ($FullSource | ?{$_.Attributes -ne 'Directory'}).Count
+    
+    $DriverFiles = $FullSource | ?{
+        ($_.Extension -eq '.bin') -or
+        ($_.Extension -eq '.cab') -or
+        ($_.Extension -eq '.cat') -or
+        ($_.Extension -eq '.dll') -or
+        ($_.Extension -eq '.inf') -or
+        ($_.Extension -eq '.ini') -or
+        ($_.Extension -eq '.oem') -or
+        ($_.Extension -eq '.sys')
+    }
+    $DestFileCount = ($DriverFiles | ?{$_.Attributes -ne 'Directory'}).Count
+    
+    foreach ($File in $DriverFiles)
+    {
+        $SourceFile = $File.FullName
+        $ReplacePath = $SourcePath -replace '\\','\\'
+        $DestDir = $SourceFile -replace ($ReplacePath, $DestPath)
+        
+        if (!(Test-Path -Path $DestDir))
+        {
+            New-Item -Path $DestDir -ItemType Directory | Out-Null
+        }
+        
+        Copy-Item -Path $SourceFile -Destination $DestDir | Out-Null
+    }
+    
+    $TotalStats = New-Object -TypeName psobject
+    $TotalStats | Add-Member -MemberType NoteProperty -Name FilesKept -Value $DestFileCount
+    $TotalStats | Add-Member -MemberType NoteProperty -Name FilesDropped -Value ($SourceFileCount - $DestFileCount)
+    $TotalStats | Add-Member -MemberType NoteProperty -Name OriginalFiles -Value $SourceFileCount
+    $TotalStats | Add-Member -MemberType NoteProperty -Name SourceSizeGB -Value (Get-FolderSize -Path $SourcePath).SizeinGB
+    $TotalStats | Add-Member -MemberType NoteProperty -Name NewSizeGB -Value (Get-FolderSize -Path $DestPath).SizeInGB
+    
+    return $TotalStats
+}
+<#
+    Example Output:
+    
+    PS C:\> Split-DriverSource -SourcePath '\\<Driversource>\Windows7x64-old' -DestPath '<Driversource>\Windows7x64'
+
+    FilesKept     : 932
+    FilesDropped  : 1526
+    OriginalFiles : 2458
+    SourceSizeGB  : 1.29
+    NewSizeGB     : 0.59
+    
+    PS C:\>
+#>
+
+#endregion
